b5f3889d63689944c96d869bd779b284c6fa12ba
aridguy <rotimiariyo@gmail.com>
2023-07-08 01:20:41 +0100

    testing paystack worked

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Payment from './Route/Payment/Payment';
+// import Payment from './Route/Payment/Payment';
+import Paystack from './Route/Signin/Paystack';
 import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <div>
     <Navbar />
-      <Payment />
+      <Paystack />
     </div>
   );
 }
